4a97b72d0089aff5a3a3e15cd4ca2cc8fa627e51
Josh Hobson <joshuahobson1995@gmail.com>
2023-09-05 16:05:58 -0600

    test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -44,7 +44,7 @@ function Home() {
 
       <div className='welcome'>
 <div className='w'>
-        <h4 className='w_1'>Welcome to</h4>
+        <h4 className='w_1'>TEST</h4>
         <h1 className='w_2'>Jericho Nursery</h1>
         <h4 className='w_3'>All you Yard and Garden needs</h4>
 </div>
